6d81f5fe33f9060caeb2ce0182d9a8975c892949
Ihsane-12 <benminaihssane@gmail.com>
2026-06-19 15:23:49 +0100

    mise en place StockService pou crer un lot

---
+++ src/Service/StockService.php
@@ -0,0 +1,28 @@
+<?php
+// src/Service/StockService.php
+
+require_once __DIR__ . '/../Repository/StockBatchRepository.php';
+require_once __DIR__ . '/../Repository/ProductRepository.php';
+
+class StockService
+{
+    private StockBatchRepository $batchRepo;
+    private ProductRepository    $productRepo;
+
+    public function __construct()
+    {
+        $this->batchRepo   = new StockBatchRepository();
+        $this->productRepo = new ProductRepository();
+    }
+    public function createBatch(int $productId, string $lotNumber, int $quantity, string $expiryDate): bool
+    {
+        $today     = new DateTime((new DateTime())->format('Y-m-d')); // BUG CORRIGÉ
+        $inputDate = new DateTime($expiryDate);
+
+        if ($inputDate < $today) {
+            return false;
+        }
+
+        return $this->batchRepo->saveInputBatch($productId, $lotNumber, $quantity, $expiryDate);
+    }
+}
